fe47ec533fea8a7e0fab3085dbd64d1dbd0e8e64
antoine-rocher <antoine.rocher@epfl.ch>
2024-08-21 08:24:13 -0700

    Stardens tests

--- CRStools/utils.py
+++ CRStools/utils.py
@@ -646,10 +646,10 @@ def plot_moll(hmap, whmap=None, min=None, max=None, nest=False, title='', label=
     m.mask=mask_2
     map_to_plot = hp.cartview(m, nest=nest, rot=rot, flip='geo', fig=1, return_projected_map=True)
 
-    if stardens:
-        f_stardens = os.path.join(os.path.dirname(__file__), 'data', 'pixweight-dr10-128-new.fits')
-        STARDENS = fitsio.FITS(f_stardens)[1]['STARDENS'][:]
-        starmap_to_plot = hp.cartview(STARDENS, nest=True, rot=rot, flip='geo', fig=1, return_projected_map=True)
+    # if stardens:
+    #     f_stardens = os.path.join(os.path.dirname(__file__), 'data', 'pixweight-dr10-128-new.fits')
+    #     STARDENS = fitsio.FITS(f_stardens)[1]['STARDENS'][:]
+    #     starmap_to_plot = hp.cartview(STARDENS, nest=True, rot=rot, flip='geo', fig=1, return_projected_map=True)
     plt.close()
 
     # build ra, dec meshgrid to plot 2d array
@@ -667,8 +667,8 @@ def plot_moll(hmap, whmap=None, min=None, max=None, nest=False, title='', label=
 
     mesh = plt.pcolormesh(np.radians(ra_grid), np.radians(dec_grid), map_to_plot, vmin=min, vmax=max, cmap=cmap, edgecolor='none', lw=0)
 
-    if stardens:
-        ttt= ax.pcolormesh(np.radians(ra_grid), np.radians(dec_grid), starmap_to_plot, vmin=5000, vmax=30000, cmap='binary', edgecolor='none', lw=0, alpha=0.5)
+    # if stardens:
+    #     ttt= ax.pcolormesh(np.radians(ra_grid), np.radians(dec_grid), starmap_to_plot, vmin=5000, vmax=30000, cmap='binary', edgecolor='none', lw=0, alpha=0.5)
         
     if mask_dir is None:
          mask_dir = os.path.join(os.path.dirname(__file__), 'mask_fp')
