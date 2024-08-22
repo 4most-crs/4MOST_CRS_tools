2170bb6fb27a9b23d24e70c5863685ec2bb2771d
antoine-rocher <antoine.rocher@epfl.ch>
2024-08-22 03:00:25 -0700

    modif color fp

--- CRStools/utils.py
+++ CRStools/utils.py
@@ -991,7 +991,7 @@ def plot_moll(hmap, whmap=None, min=None, max=None, nest=False, title='', label=
         ra = np.remainder(ra + 360, 360)
         ra[ra > 180] -= 360
         #plt.plot(ra[ra.argsort()],dec[ra.argsort()], lw=0.8, color='lightskyblue')
-        plt.fill_between(np.radians(ra[ra.argsort()]),np.radians(dec[ra.argsort()]), -np.pi/2, interpolate=True, color=colors[0], alpha=0.2, label='LSST')
+        plt.fill_between(np.radians(ra[ra.argsort()]),np.radians(dec[ra.argsort()]), -np.pi/2, interpolate=True, color=colors[8], alpha=0.2, label='LSST')
 
 
     if euclid_fp:
